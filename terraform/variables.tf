75cf0d7a9bafc15181515b5515c60c556ad0d523
ashish arora <ashisharora24@outlook.com>
2025-08-30 07:43:31 +0530

    feat: adding terraform and publishing the result

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "client_code" {
+    description     = "client code for resource naming"
+    type            = string
+}
+
+variable "location" {
+    description     = "client azure location"
+    type            = string
+    default         = "Central India"
+}

7be75a47537a646dbe82c0da2b79698d30ef2096
ashish arora <ashisharora24@outlook.com>
2025-08-30 08:11:54 +0530

    adding naming of the variable

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,14 +1,14 @@
-resource "azurerm_resource_group"{
+resource "azurerm_resource_group" "core" {
     name        = "rg-${var.client_code}-core"
     location    =  var.location
 }
 
-resource "azurerm_resource_group" {
+resource "azurerm_resource_group" "jump" {
     name        = "rg-${var.client_code}-jump"
     location    = var.location
 }
 
-resource "azurerm_resource_group" {
+resource "azurerm_resource_group" "agent" {
     name        = "rg-${var.client_code}-agent"
     location    = var.location
 }
